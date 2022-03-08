5b861a670193ba2a4da503c13c57c06c08a3f9e8
Marco Aurélio Mendes <68308754+marcomendesbidula@users.noreply.github.com>
2022-03-07 23:31:58 -0300

    Implementada Direcao

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,4 +1,5 @@
 class Pessoa:
+    olhos = 2
     def __init__(self, *filhos, nome=None, idade=40):
         self.idade = idade
         self.nome = nome
@@ -6,6 +7,14 @@ class Pessoa:
 
     def cumprimentar(self):
         return f'Olá {id(self)}'
+
+    @staticmethod
+    def metodo_estatico():
+        return 42
+
+    @classmethod
+    def nome_e_atributos_de_classe(cls):
+        return f'{cls} - olhos {cls.olhos}'
 
 if __name__ == '__main__':
     marco = Pessoa(nome='Marco')
@@ -19,5 +28,14 @@ if __name__ == '__main__':
        print(filho.nome)
     mateus.sobrenome = 'Mendes'
     del mateus.filhos
+    mateus.olhos = 1
+    del mateus.olhos
     print(mateus.__dict__)
     print(marco.__dict__)
+    Pessoa.olhos = 3
+    print(Pessoa.olhos)
+    print(mateus.olhos)
+    print(marco.olhos)
+    print(id(Pessoa.olhos), id(mateus.olhos), id(marco.olhos))
+    print(Pessoa.metodo_estatico(), mateus.metodo_estatico())
+    print(Pessoa.nome_e_atributos_de_classe(), mateus.nome_e_atributos_de_classe())
